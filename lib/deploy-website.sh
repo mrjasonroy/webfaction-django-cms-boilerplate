bbf24f7ea945657cea00d68d53fbe2597a191cc2
Martin Brochhaus <mbrochh@gmail.com>
2011-02-17 16:54:53 +0100

    deploy-website can now skip syncdb and migration when argument is supplied

--- lib/deploy-website.sh
+++ lib/deploy-website.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
+# if the first Argument is not set to 1, syncdb and migrate will be executed
 cd ~/src/website_src
 git pull
 rsync -avz --stats --delete --exclude-from=$HOME/bin/rsync-excludes-django.txt ~/src/website_src/webapps/django/project ~/webapps/django/
 rsync -avz --stats --delete --exclude-from=$HOME/bin/rsync-excludes-static.txt ~/src/website_src/webapps/static/ ~/webapps/static/
 cd ~/webapps/django/project
-python2.6 manage.py syncdb
-python2.6 manage.py migrate
-cd ~/webapps/django/apache2/bin
-./restart
+if [ $1 ]; then
+	:
+else
+	python2.6 manage.py syncdb
+	python2.6 manage.py migrate
+        cd ~/webapps/django/apache2/bin
+	./restart
+fi
 exit 0
 
